e1381f43cbbbc5236845ca11726f866a7e0ce5d3
GaborZeller <gabriel.d.celery@gmail.com>
2024-08-24 12:47:50 +0100

    feat: changed neovim theme from Macchiato to Mocha

--- wezterm_configs/.wezterm.skel.wsl.lua
+++ wezterm_configs/.wezterm.skel.wsl.lua
@@ -12,7 +12,7 @@ config.automatically_reload_config = true
 config.default_domain = "WSL:Ubuntu"
 
 -- use catppuccin theme
-config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = "Catppuccin Mocha"
 
 -- disable ligatures
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
