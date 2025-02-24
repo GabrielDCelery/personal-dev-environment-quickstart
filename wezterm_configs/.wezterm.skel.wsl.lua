8491444050b0c3a3880c39d95116353acd076944
GaborZeller <gabriel.d.celery@gmail.com>
2025-02-24 23:42:28 +0000

    feat: updated wezterm skeleton file

--- wezterm_configs/.wezterm.skel.wsl.lua
+++ wezterm_configs/.wezterm.skel.wsl.lua
@@ -8,14 +8,36 @@ end
 -- automatically reload config
 config.automatically_reload_config = true
 
--- make wezterm load wsl
+config.enable_tab_bar = false
+
+config.max_fps = 120
+
 config.default_domain = "WSL:Ubuntu"
 
 -- use catppuccin theme
 config.color_scheme = "Catppuccin Mocha"
 
--- disable ligatures
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+-- set font
+-- config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font({
+	family = "JetBrains Mono",
+	-- disable ligatures
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+})
+
+-- window padding
+config.window_padding = {
+	bottom = "0px",
+}
+
+config.audible_bell = "Disabled"
+
+config.font_size = 11
+
+config.colors = {
+	-- background = "#181825",
+	background = "#11111b",
+}
 
 -- and finally, return the configuration to wezterm
 return config
